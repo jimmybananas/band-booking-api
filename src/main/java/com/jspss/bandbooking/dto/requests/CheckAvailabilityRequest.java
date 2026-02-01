4d849098c3de38f511443c959b6ff27923fa560d
jimmybananas <jimmothy.greenwood@gmail.com>
2026-02-01 16:59:44 -0700

    Updated all DTO's with Swagger Schema annotations.

--- src/main/java/com/jspss/bandbooking/dto/requests/CheckAvailabilityRequest.java
+++ src/main/java/com/jspss/bandbooking/dto/requests/CheckAvailabilityRequest.java
@@ -1,10 +1,18 @@
 package com.jspss.bandbooking.dto.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.ZonedDateTime;
 
+@Schema(description = "Request body for checking band availability.")
 public record CheckAvailabilityRequest(
+
+        @Schema(description = "Band ID", example = "45")
         Long bandId,
+        @Schema(description = "Booking start time.", example = "2026-03-15T09:00:00Z")
         ZonedDateTime start,
+
+        @Schema(description = "Booking end time.", example = "2026-03-15T11:00:00Z")
         ZonedDateTime end
 ) {
 }
